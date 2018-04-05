80d02d5aa75022d917f5ce128ecc5fbd524baed4
Anatolii_Syunyaev <Anatolii_Syunyaev@epam.com>
2018-04-05 18:41:55 +0300

    EPMLSTRTAI-718 fix test

--- src/test/java/com/epam/test_generator/entity/TokenTest.java
+++ src/test/java/com/epam/test_generator/entity/TokenTest.java
@@ -3,16 +3,13 @@ package com.epam.test_generator.entity;
 import com.epam.test_generator.entities.Token;
 import com.epam.test_generator.entities.User;
 import org.junit.Test;
-
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TokenTest {
 
     private final int LONG_EXPIRATION_DURATION = 60;
-    private final int ZERO_EXPIRATION_DURATION = 0;
+    private final int ZERO_EXPIRATION_DURATION = -1;
 
     @Test
     public void isExpired_NotExpiredToken_False() {
